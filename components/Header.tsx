b6c1385b510b7cc4ebc463da79b38a60ce64eabd
Abdullah Alojado <Abed>
2023-12-22 14:00:17 -0500

    12-22

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,14 +7,30 @@ import { RxCaretLeft, RxCaretRight } from 'react-icons/rx';
 import { HiHome } from 'react-icons/hi';
 import { BiSearch } from 'react-icons/bi';
 import Button from './Button';
+import useAuthModal from '@/hooks/useAuthModal';
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { useUser } from '@/hooks/useUser';
 
 interface HeaderProps {
   children: React.ReactNode;
   className?: string;
 }
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const authModal = useAuthModal();
   const router = useRouter();
-  const handleLogout = () => {};
+  const supabaseClient = useSupabaseClient();
+  const { user } = useUser();
+
+  const handleLogout = async () => {
+    const error = await supabaseClient.auth.signOut();
+    // TODO: Reset any playing songs
+    router.refresh();
+
+    if (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div
       className={twMerge(
@@ -24,18 +40,30 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
     >
       <div className=' w-full mb-4 flex items-center justify-between'>
         <div className=' hidden md:flex gap-x-2 items-center'>
-          <button className=' rounded-full bg-black flex items-center justify-center hover:opacity-75 transition'>
+          <button
+            title='left'
+            className=' rounded-full bg-black flex items-center justify-center hover:opacity-75 transition'
+          >
             <RxCaretLeft className='text-white' size={35} />
           </button>
-          <button className=' rounded-full bg-black flex items-center justify-center hover:opacity-75 transition'>
+          <button
+            title='right'
+            className=' rounded-full bg-black flex items-center justify-center hover:opacity-75 transition'
+          >
             <RxCaretRight className='text-white' size={35} />
           </button>
         </div>
         <div className=' flex md:hidden gap-x-2 items-center'>
-          <button className=' rounded-full p-2 bg-white flex items-center hover:opacity-75 transition'>
+          <button
+            title='home'
+            className=' rounded-full p-2 bg-white flex items-center hover:opacity-75 transition'
+          >
             <HiHome className='text-black' size={20} />
           </button>
-          <button className=' rounded-full p-2 bg-white flex items-center hover:opacity-75 transition'>
+          <button
+            title='search'
+            className=' rounded-full p-2 bg-white flex items-center hover:opacity-75 transition'
+          >
             <BiSearch className='text-black' size={20} />
           </button>
         </div>
@@ -43,14 +71,17 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
           <>
             <div>
               <Button
-                onClick={() => {}}
+                onClick={authModal.onOpen}
                 className=' bg-transparent text-neutral-300 font-md'
               >
                 Sign up
               </Button>
             </div>
             <div>
-              <Button onClick={() => {}} className=' bg-white px-6 py-2'>
+              <Button
+                onClick={authModal.onOpen}
+                className=' bg-white px-6 py-2'
+              >
                 Log in
               </Button>
             </div>
