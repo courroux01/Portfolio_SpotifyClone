6e141ccb311b63f4dc2d947fdaa45a2a36142dbb
Abdullah Alojado <Abed>
2024-01-12 10:39:37 -0500

    h

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -9,7 +9,7 @@ export const revalidate = 0;
 export default async function Home() {
   const songs = await getSongs();
   return (
-    <div className='  bg-neutral-900 rounded-lg overflow-hidden overflow-y-auto'>
+    <div className='  bg-neutral-900 h-full w-full rounded-lg overflow-hidden overflow-y-auto'>
       <Header>
         <div className='mb-2'>
           <h1 className='text-white text-3xl font-semibold'>Welcome back!</h1>
